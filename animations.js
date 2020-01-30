c7683456a9ca0ab2771d41bb546b2ac4ffe8731f
amberchoudhry <ac08221@sjfc.edu>
2020-01-30 01:12:24 -0500

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,50 @@
+$( document ).ready(function() {
+    $("#work1").on('click', function() {
+        $('#c1').stop().fadeToggle();
+        $('#c1 p').css('display','block');
+    });
+        $("#work2").on('click', function() {
+        $('#c2').stop().fadeToggle();
+        $('#c2 p').css('display','block');
+    });
+        $("#work3").on('click', function() {
+        $('#c3').stop().fadeToggle();
+        $('#c3 p').css('display','block');
+    });
+        $("#work4").on('click', function() {
+        $('#c4').stop().fadeToggle();
+        $('#c4 p').css('display','block');
+    });
+        $("#work5").on('click', function() {
+        $('#c5').stop().fadeToggle();
+        $('#c5 p').css('display','block');
+    });
+        $("#work6").on('click', function() {
+        $('#c6').stop().fadeToggle();
+        $('#c6 p').css('display','block');
+    });
+        $("#work7").on('click', function() {
+        $('#c7').stop().fadeToggle();
+        $('#c7 p').css('display','block');
+    });
+        $("#work8").on('click', function() {
+        $('#c8').stop().fadeToggle();
+        $('#c8 p').css('display','block');
+    });
+        $("#work9").on('click', function() {
+        $('#c9').stop().fadeToggle();
+        $('#c9 p').css('display','block');
+    });
+        $("#work10").on('click', function() {
+        $('#c10').stop().fadeToggle();
+        $('#c10 p').css('display','block');
+    });
+        $("#work11").on('click', function() {
+        $('#c11').stop().fadeToggle();
+        $('#c11 p').css('display','block');
+    });
+        $("#work12").on('click', function() {
+        $('#c12').stop().fadeToggle();
+        $('#c12 p').css('display','block');
+    });
+});
